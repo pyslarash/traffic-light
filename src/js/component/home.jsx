ff6814e2543664e5b1ab5215d4be929a73e12d4c
pyslarash <alex.pyslarash@gmail.com>
2023-01-25 17:33:40 +0000

    Initial commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const [selectedColor, setSelectedColor] = useState("red"); // Setting up useState to change the color
+	
+	useEffect(() => {
+		const timer = setTimeout(() => {				// Changing the light every 3 seconds
+			if (selectedColor === "red") {
+				setSelectedColor("yellow")
+			} else if (selectedColor === "yellow") {
+				setSelectedColor("green")
+			} else if (selectedColor === "green") {
+				setSelectedColor("red")
+			}
+		}, 3000);
+		return () => clearTimeout(timer);			// This line makes sure that we won't have an issue with light running randomply when clicking on another one. The "return" cleans up the effect when it unmounts.
+	});	
+		
+	return (										// Here we are utilizing the function where we can click on each individual light
+		<>
+		<div className="traffic-holder"></div>		
+		<div className="traffic-light">
+		<div onClick={() => setSelectedColor("red")} className={"light red" + (selectedColor === "red" ? " glow" : "")}></div>
+		<div onClick={() => setSelectedColor("yellow")} className={"light yellow" + (selectedColor === "yellow" ? " glow" : "")}></div>
+		<div onClick={() => setSelectedColor("green")} className={"light green" + (selectedColor === "green" ? " glow" : "")}></div>
 		</div>
+		</>
 	);
 };
 
